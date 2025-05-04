fb4e05ebe7e6f9fe018116c6f2dd3c11918b0073
Kone Abdallah Hakim <71593784+schalappe@users.noreply.github.com>
2025-05-04 16:44:59 +0200

    feat: création d'un système de gestion des variables d'environnement

---
+++ backend/app/config.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Module containing configuration class.
+"""
+
+from os import environ
+
+class Config:
+    def __init__(self):
+        """
+        Initialize a new configuration object.
+
+        The environment variable ENV is used to select between the development and production environments.
+        If ENV is not set, the development environment is assumed.
+        """
+        self.env = environ.get("ENV", "dev")
+
+    def get_db_url(self):
+        """
+        Return the URL of the DuckDB database to use.
+
+        Returns
+        -------
+        str
+            The URL of the DuckDB database to use.
+        
+        Raises
+        ------
+        ValueError
+            If the environment is invalid.
+        """
+        if self.env == "dev":
+            return "duckdb://:memory:"
+        if self.env == "prod":
+            return environ.get("DB_URL")
+        raise ValueError("Invalid environment")
+
+config = Config()
