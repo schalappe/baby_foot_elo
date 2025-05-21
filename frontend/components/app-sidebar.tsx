9c168882cc3859bcdaa1062e0c4d126d7d6d686a
Kone Abdallah Hakim <71593784+schalappe@users.noreply.github.com>
2025-05-21 05:17:56 +0200

    style: adjust component spacing and icon sizing in header and sidebar

--- frontend/components/app-sidebar.tsx
+++ frontend/components/app-sidebar.tsx
@@ -39,7 +39,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Command className="size-4" />
+                  <Command className="size-5" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">Baby Foot ELO</span>
